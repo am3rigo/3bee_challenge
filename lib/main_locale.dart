6e6f3de707a2ed91bd8d241c9d7c4faa02e19aa4
am3rigo <byamerigo@icloud.com>
2023-03-10 19:08:37 +0100

    created main_locale for mock repositories

---
+++ lib/main_locale.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:threebee_challenge/app.dart';
+import 'package:threebee_challenge/bootstrap.dart';
+
+void main() async {
+  await bootstrap();
+  runApp(const MyApp());
+}
